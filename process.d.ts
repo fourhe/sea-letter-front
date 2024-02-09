223025bf505ed08617eed6627ab7aae2077a546f
sangwook <bnbt3@naver.com>
2024-02-09 20:47:59 +0900

    feat: 센트리 초기 설치

--- process.d.ts
+++ process.d.ts
@@ -3,5 +3,7 @@ declare namespace NodeJS {
     NEXT_PUBLIC_SERVER_URL: string;
     NEXT_PUBLIC_REVIEW_URL: string;
     NEXT_PUBLIC_GA_MEASUREMENT_ID: string;
+    NEXT_PUBLIC_SENTRY_DSN: string;
+    SENTRY_AUTH_TOKEN: string;
   }
 }
