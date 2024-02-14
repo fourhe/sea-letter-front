4f8da349a4eedc50ede7d021dc9725754e0a5c9e
sangwook <bnbt3@naver.com>
2024-02-14 21:21:05 +0900

    feat: 모바일 유저 체크를 위한 미들웨어 작성

---
+++ src/app/desktop/page.tsx
@@ -0,0 +1,7 @@
+const Page = () => (
+  <div>
+    <h1>desktop</h1>
+  </div>
+);
+
+export default Page;
--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,17 @@ import {cookies} from 'next/headers';
 import type {NextRequest} from 'next/server';
 import {NextResponse} from 'next/server';
 
+import {isMobileUserAgent} from '@/utils/userAgent';
+
 export const middleware = (request: NextRequest) => {
+  if (request.nextUrl.pathname.startsWith('/auth')) {
+    const userAgent = request.headers.get('user-agent')!;
+    const isMobile = isMobileUserAgent(userAgent);
+    if (isMobile || process.env.NODE_ENV === 'development') {
+      return NextResponse.next();
+    }
+    return NextResponse.redirect(new URL('/desktop', request.url));
+  }
   const isRefreshToken = cookies().has('refresh-token');
   if (isRefreshToken) {
     return NextResponse.next();
@@ -11,5 +21,5 @@ export const middleware = (request: NextRequest) => {
 };
 
 export const config = {
-  matcher: '/main/:path*',
+  matcher: ['/main/:path*', '/auth/:path*'],
 };
