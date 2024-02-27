8aa31fe517d61ee767abeca994fe645935fef2ab
sangwook <bnbt3@naver.com>
2024-02-28 01:35:14 +0900

    feat: 쿠키 maxAge 설정

--- src/app/api/auth/callback/kakao/route.ts
+++ src/app/api/auth/callback/kakao/route.ts
@@ -8,8 +8,11 @@ export const GET = async (request: NextRequest) => {
   const code = request.nextUrl.searchParams.get('code');
   if (!code) throw new Error('No code');
   const {accessToken, refreshToken} = await AuthenticationService.logIn(code);
-  cookies().set('access-token', accessToken);
-  cookies().set('refresh-token', refreshToken, {httpOnly: true});
+  cookies().set('access-token', accessToken, {maxAge: 60 * 60 * 3});
+  cookies().set('refresh-token', refreshToken, {
+    maxAge: 60 * 60 * 24 * 14,
+    httpOnly: true,
+  });
 
   return NextResponse.redirect(new URL('/', request.url));
 };
