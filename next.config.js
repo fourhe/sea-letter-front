838ddf2eac78b4455df92530004b5ad2acb019e9
sangwook <bnbt3@naver.com>
2024-03-30 22:28:24 +0900

    feat: axios class로 전부 변환

--- next.config.js
+++ next.config.js
@@ -11,6 +11,16 @@ const nextConfig = {
     });
     return config;
   },
+  experimental: {
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
+      },
+    },
+  },
 };
 
 module.exports = nextConfig;
